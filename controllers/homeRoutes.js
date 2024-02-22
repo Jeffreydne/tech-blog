36b858e210a6029aa0c6a1f752e89ac1ccedef14
Jeffreydne <Jeffreydne@gmail.com>
2024-02-22 12:00:15 -0800

    change homeRoute to views/homepage.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,26 @@
 const router = require('express').Router();
 const path = require('path');
 
+const { Blog } = require('../models');
+
 // This is the 'get' route 
 router.get('/', async (req, res) => {
-  // Here, index.html is rendered
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  try {
+    const blogData = await Blog.findAll();
+
+  const blogs = blogData.map((blog) =>
+    blog.get({ plain: true })
+  );
+
+  res.render('homepage', {
+    blogs,
+  });
+  } catch (err) {
+    console.log('This is the error: ' + err);
+    res.status(500).json(err);
+  }
+  // Here, index.html is rendered- commented out to instead use handlebars
+  // res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3011;
+const PORT = process.env.PORT || 3012;
 
 // set up and use sessions with sess object as argument
 const sess = {
