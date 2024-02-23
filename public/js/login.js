7ffd5ac4f371335bd1703725d8895055ec30f12a
Jeffreydne <Jeffreydne@gmail.com>
2024-02-23 10:38:15 -0800

    add public/js/login.js file

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#emailLogin').value.trim();
+    const password = document.querySelector('#passwordLogin').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in.');
+      }
+    }
+  };
+  
+  const signupHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#userName').value.trim();
+    const email = document.querySelector('#emailSignup').value.trim();
+    const password = document.querySelector('#passwordSignup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupHandler);
