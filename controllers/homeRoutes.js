e0037af5a49d588268288b54ebbbb6435da9b8f5
Jeffreydne <Jeffreydne@gmail.com>
2024-02-23 10:03:27 -0800

    add login page header

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const { Blog } = require('../models');
 
-// This is the 'get' route 
+// This is the GET route for the home page
 router.get('/', async (req, res) => {
   try {
     const blogData = await Blog.findAll();
@@ -20,8 +20,30 @@ router.get('/', async (req, res) => {
     console.log('This is the error: ' + err);
     res.status(500).json(err);
   }
-  // Here, index.html is rendered- commented out to instead use handlebars
-  // res.sendFile(path.join(__dirname, '../views/index.html'));
+ 
+});
+
+// This is the GET route for the login page
+router.get('/login', async (req, res) => {
+
+   // Here, login.handlebars is rendered- as the layout template is not needed
+  //  document.location.replace('../views/index.html');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+  // try {
+  //   const blogData = await Blog.findAll();
+
+  // const blogs = blogData.map((blog) =>
+  //   blog.get({ plain: true })
+  // );
+
+  // res.render('homepage', {
+  //   blogs,
+  // });
+  // } catch (err) {
+  //   console.log('This is the error: ' + err);
+  //   res.status(500).json(err);
+  // }
+  
 });
 
 module.exports = router;
