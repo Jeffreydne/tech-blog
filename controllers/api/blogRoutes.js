ad6c4c64952db64a8a8b5d7b9cf3450ec279ea31
Jeffreydne <Jeffreydne@gmail.com>
2024-02-23 20:46:09 -0800

    addblogRoutes.js & associated funtionality

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req,res) => {
+    try {
+        const blogData = await Blog.findAll();
+    
+      const blogs = blogData.map((blog) =>
+        blog.get({ plain: true })
+      );
+    
+      res.render('homepage', {
+        blogs,
+      });
+      } catch (err) {
+        console.log('This is the error: ' + err);
+        res.status(500).json(err);
+      }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const newPost = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
